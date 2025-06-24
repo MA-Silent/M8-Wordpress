69013e9a8bec4a3c19ab4be27478f97b4c905c88
MA-Silent <38739@ma-web.nl>
2025-06-24 11:16:19 +0200

    Les 7 Done

--- themes/custom/PostType.php
+++ themes/custom/PostType.php
@@ -30,5 +30,33 @@ $args = [
 	'show_in_rest' => true,
 ];
 
+$labels = [
+    'name'              => _x( 'Origins', 'taxonomy general name', 'Text_Domain' ),
+	'singular_name'     => _x( 'Origin', 'taxonomy singular name', 'Text_Domain' ),
+	'search_items'      => __( 'Search Origins', 'Text_Domain' ),
+	'popular_items'     => __( 'Popular Origins', 'Text_Domain' ),
+	'all_items'         => __( 'All Origins', 'Text_Domain' ),
+	'parent_item'       => __( 'Parent Origin', 'Text_Domain' ),
+	'parent_item_colon' => __( 'Parent Origin:', 'Text_Domain' ),
+	'edit_item'         => __( 'Edit Origin', 'Text_Domain' ),
+	'update_item'       => __( 'Update Origin', 'Text_Domain' ),
+	'add_new_item'      => __( 'Add New Origin', 'Text_Domain' ),
+	'new_item_name'     => __( 'New Origin Name', 'Text_Domain' ),
+	'menu_name'         => __( 'Origins', 'Text_Domain' ),
+];
+
 register_post_type('wood', $args);
+
+$args = [
+	'hierarchical'      => true,
+	'labels'            => $labels,
+	'show_ui'           => true,
+	'show_admin_column' => true,
+	'query_var'         => true,
+	'show_in_rest'      => true,
+	'rewrite'           => [ 'slug' => 'origin' ],
+];
+
+register_taxonomy( 'origin', 'wood', $args );
+
 require_once('PostType_rand.php');
--- themes/custom/PostType_rand.php
+++ themes/custom/PostType_rand.php
@@ -30,4 +30,31 @@ $args = [
 	'show_in_rest' => true,
 ];
 
-register_post_type('Rand', $args);
+$labels = [
+    'name'              => _x( 'Origins', 'taxonomy general name', 'Text_Domain' ),
+	'singular_name'     => _x( 'Origin', 'taxonomy singular name', 'Text_Domain' ),
+	'search_items'      => __( 'Search Origins', 'Text_Domain' ),
+	'popular_items'     => __( 'Popular Origins', 'Text_Domain' ),
+	'all_items'         => __( 'All Origins', 'Text_Domain' ),
+	'parent_item'       => __( 'Parent Origin', 'Text_Domain' ),
+	'parent_item_colon' => __( 'Parent Origin:', 'Text_Domain' ),
+	'edit_item'         => __( 'Edit Origin', 'Text_Domain' ),
+	'update_item'       => __( 'Update Origin', 'Text_Domain' ),
+	'add_new_item'      => __( 'Add New Origin', 'Text_Domain' ),
+	'new_item_name'     => __( 'New Origin Name', 'Text_Domain' ),
+	'menu_name'         => __( 'Origins', 'Text_Domain' ),
+];
+
+register_post_type('rand', $args);
+
+$args = [
+	'hierarchical'      => true,
+	'labels'            => $labels,
+	'show_ui'           => true,
+	'show_admin_column' => true,
+	'query_var'         => true,
+	'show_in_rest'      => true,
+	'rewrite'           => [ 'slug' => 'origin' ],
+];
+
+register_taxonomy( 'origin', 'rand', $args );
